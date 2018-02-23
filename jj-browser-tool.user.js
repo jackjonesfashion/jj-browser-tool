9e8d3454783678f694359c7a24a8f8776fce58ee
steffen-simonsen <steffen.simonsen@bestseller.com>
2018-02-23 12:58:03 +0100

    Update jj-browser-tool.user.js

--- jj-browser-tool.user.js
+++ jj-browser-tool.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jj-browser-tool
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  BING Language selector
 // @author       Jack & Jones Ecommerce
 // @include      *.jackjones.com*
