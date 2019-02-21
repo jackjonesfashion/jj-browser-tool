571d6b0aa00d60ddd4ec730af592cbbaf7a1f686
fennefoss <mfl@fennefoss.dk>
2019-02-21 11:05:48 +0100

    Version 1.5

--- jj-browser-tool.user.js
+++ jj-browser-tool.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jj-browser-tool
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @description  BING Language selector
 // @author       Jack & Jones Ecommerce
 // @include      *.jackjones.com*
