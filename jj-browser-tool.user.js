8b7cd582ebbd391d3d56cf357c4df14e7cffa81e
urbasiusedvinas <edvinas.urbasius@bestseller.com>
2019-09-30 12:11:05 +0200

    version number increased

--- jj-browser-tool.user.js
+++ jj-browser-tool.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jj-browser-tool
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      2.1
 // @description  BING Language selector
 // @author       Jack & Jones Ecommerce
 // @include      *.onlyandsons.com*
